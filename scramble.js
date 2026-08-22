577592bca93c3e063372f2e7e7aee74e45770a11
sereefwe <sereefwe@gmail.com>
2026-08-22 22:48:50 +0900

    Use identical crackle pace for activities list as the rest of the page text

--- scramble.js
+++ scramble.js
@@ -39,37 +39,16 @@
     }, delay || 0);
   }
 
-  var NORMAL_DURATION = 2600;
-  var NORMAL_STAGGER = 300;
-  var FAST_DURATION = 320;
-  var FAST_CHANGE_INTERVAL = 35;
-
   function init() {
     var targets = document.querySelectorAll("[data-scramble], [data-scramble-fast]");
     if (reducedMotion) return;
 
-    var normalEls = [];
-    var fastEls = [];
     for (var i = 0; i < targets.length; i++) {
       var el = targets[i];
       var finalText = el.textContent;
       el.textContent = finalText.replace(/\S/g, randChar);
-      if (el.hasAttribute("data-scramble-fast")) {
-        fastEls.push({ el: el, text: finalText });
-      } else {
-        normalEls.push({ el: el, text: finalText });
-      }
+      crackle(el, finalText, 2600, 50, i * 300);
     }
-
-    var normalFinish = normalEls.length > 0 ? (normalEls.length - 1) * NORMAL_STAGGER + NORMAL_DURATION : 0;
-    var fastStagger = fastEls.length > 1 ? Math.max(15, (normalFinish - FAST_DURATION) / (fastEls.length - 1)) : 0;
-
-    normalEls.forEach(function (item, i) {
-      crackle(item.el, item.text, NORMAL_DURATION, 50, i * NORMAL_STAGGER);
-    });
-    fastEls.forEach(function (item, i) {
-      crackle(item.el, item.text, FAST_DURATION, FAST_CHANGE_INTERVAL, i * fastStagger);
-    });
   }
 
   if (document.readyState === "loading") {
